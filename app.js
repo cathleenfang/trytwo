e4d724bd53674e5318e06a4834300e8083c06567
zoumeifang <631737578@qq.com>
2016-11-25 20:14:04 +0800

    设置路由

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const koa = require('koa'),
+      path = require('path'),
+      serve = require('koa-static'),
+      router = require('./routes/router'),
+      Pug = require('koa-pug');
+
+const app = koa();
+
+const devPath = path.resolve(__dirname, './dev');
+
+// view engine
+const pug = new Pug({
+  viewPath: './dev/pugs',
+  basedir: './dev/pugs',
+  debug: app.env !== 'production',
+  locals: {
+    development: app.env === 'development',
+    production: app.env === 'production'
+  },
+  app: app
+});
+
+//static
+app.use(serve(devPath));
+
+//router
+app.use(router.routes());
+
+app.listen(8080);
